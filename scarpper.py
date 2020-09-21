2e5eaabb42fa636b01310cdf9cf8ae69e6c635a4
Twinkletoes5 <developer@twinkletoes.tech>
2020-09-20 23:44:37 -0700

    Able to launch Web browser
    Added driver directly to $PATH Sovled the problem, can't use "path =
    'geckodriver'" for some reason

---
+++ scarpper.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+# path = "geckodriver"
+
+browser = webdriver.Firefox()#/usr/local/bin
+
+url = "https://teams.microsoft.com/"
+browser.get(url)
+
+Email = "ding-han.yan@student.bodwell.edu"
+
